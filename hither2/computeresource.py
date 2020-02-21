84c6499c5f2cd1673423da58c6f2a8aa83f7c5f3
Jeremy Magland <jmagland@flatironinstitute.org>
2020-02-21 16:07:38 +0000

    developing remote compute resource

--- hither2/computeresource.py
+++ hither2/computeresource.py
@@ -1,17 +1,19 @@
-import traceback
 import time
 import kachery as ka
-from .core import config, _run_serialized_job_in_container, _serialize_item
+from .core import _serialize_item, _deserialize_job, _prepare_container
+from ._util import _random_string
 
 class ComputeResource:
-    def __init__(self, mongo_url, database, compute_resource_id):
+    def __init__(self, mongo_url, database, compute_resource_id, job_handler):
         self._mongo_url = mongo_url
         self._database = database
         self._compute_resource_id = compute_resource_id
-        self._jobs = dict()
+        self._instance_id = _random_string(15)
         self._client = None
         self._client_db_url = None
         self._iterate_timer = time.time()
+        self._job_handler = job_handler
+        self._jobs = dict()
     def clear(self):
         db = self._get_db()
         db.remove(dict(
@@ -25,47 +27,166 @@ class ComputeResource:
         elapsed = time.time() - self._iterate_timer
         if elapsed < 3:
             return
+
+        self._report_active()
+        active_job_handler_ids = self._get_active_job_handler_ids()
+
         self._iterate_timer = time.time()
         db = self._get_db()
+
+        # Handle pending jobs
         query = dict(
             compute_resource_id=self._compute_resource_id,
             last_modified_by_compute_resource=False,
-            status='queued'
+            status='queued',
+            compute_resource_status='pending'
         )
         for doc in db.find(query):
-            try:
-                job_serialized = doc['job_serialized']
-                job_serialized['code'] = ka.load_object(job_serialized['code'])
-                success, result, runtime_info = _run_serialized_job_in_container(job_serialized)
-                if success:
-                    status = 'finished'
-                    exception = None
-                else:
-                    status = 'error'
-                    exception = 'test-exception'
-            except:
-                traceback.print_exc()
-                status = 'error'
-                success = False
-                result = None
-                runtime_info = None
-                exception = 'unexpected problem'
+            self._handle_pending_job(doc)
+        
+        # Handle jobs
+        job_ids = list(self._jobs.keys())
+        for job_id in job_ids:
+            job = self._jobs[job_id]
+            reported_status = getattr(job, '_reported_status')
             filter0 = dict(
                 compute_resource_id=self._compute_resource_id,
-                job_id=doc['job_id']
+                job_id=job_id
             )
-            update = {
-                '$set': dict(
-                    status=status,
-                    result=_serialize_item(result),
-                    runtime_info=runtime_info,
-                    exception=exception,
-                    last_modified_by_compute_resource=True
-                )
-            }
-            db.update_one(filter0, update=update)
+            if job._status == 'running':
+                if reported_status != 'running':
+                    print(f'Job running: {job_id}')
+                    update = {
+                        '$set': dict(
+                            status='running',
+                            compute_resource_status='running',
+                            last_modified_by_compute_resource=True
+                        )
+                    }
+                    db.update_one(filter0, update=update)
+                    setattr(job, '_reported_status', 'running')
+            elif job._status == 'finished':
+                print(f'Job finished: {job_id}')
+                update = {
+                    '$set': dict(
+                        status='finished',
+                        compute_resource_status='finished',
+                        result=_serialize_item(job._result),
+                        runtime_info=job._runtime_info,
+                        exception=None,
+                        last_modified_by_compute_resource=True
+                    )
+                }
+                db.update_one(filter0, update=update)
+                del self._jobs[job_id]
+            elif job._status == 'error':
+                print(f'Job error: {job_id}')
+                update = {
+                    '$set': dict(
+                        status='error',
+                        compute_resource_status='error',
+                        result=None,
+                        runtime_info=job._runtime_info,
+                        exception='test-exception',
+                        last_modified_by_compute_resource=True
+                    )
+                }
+                db.update_one(filter0, update=update)
+                del self._jobs[job_id]
+            
+            # check if handler is still active
+            if job_id in self._jobs:
+                handler_id = getattr(job, '_handler_id')
+                if handler_id not in active_job_handler_ids:
+                    print(f'Removing job because client handler is no longer active: {job_id}')
+                    self._job_handler.cancel_job(job_id)
+                    db.remove(filter0)
+                    del self._jobs[job_id]
+        
+        self._job_handler.iterate()
     
-    def _get_db(self):
+    def _get_active_job_handler_ids(self):
+        db = self._get_db(collection='active_job_handlers')
+
+        # remove the expired
+        t0 = _utctime() - 10
+        query = dict(
+            utctime={'$lt': t0}
+        )
+        db.remove(query)
+
+        # return handler ids for those that were not removed
+        return [doc['handler_id'] for doc in db.find({})]
+    
+    def _handle_pending_job(self, doc):
+        job_id = doc["job_id"]
+        print(f'Queuing job: {job_id}')
+        
+        try:
+            job_serialized = doc['job_serialized']
+            job_serialized['code'] = ka.load_object(job_serialized['code'])
+            container = job_serialized['container']
+            if container is None:
+                raise Exception('Cannot run serialized job outside of container.')
+            _prepare_container(container)
+        except Exception as e:
+            print(f'Error handing pending job: {job_id}')
+            print(e)
+            self._mark_job_as_error(doc, exception=e, runtime_info=None)
+            return
+        
+        job = _deserialize_job(job_serialized)
+        self._jobs[job_id] = job
+        self._job_handler.handle_job(job)
+        db = self._get_db()
+        filter0 = dict(
+            compute_resource_id=self._compute_resource_id,
+            job_id=doc['job_id']
+        )
+        update = {
+            '$set': dict(
+                compute_resource_status='queued',
+                last_modified_by_compute_resource=True
+            )
+        }
+        db.update_one(filter0, update=update)
+        setattr(job, '_reported_status', 'queued')
+        setattr(job, '_handler_id', doc['handler_id'])
+    
+    def _mark_job_as_error(self, doc, exception, runtime_info):
+        job_id = doc['job_id']
+        print(f'Job error: {job_id}')
+        db = self._get_db()
+        filter0 = dict(
+            compute_resource_id=self._compute_resource_id,
+            job_id=doc['job_id']
+        )
+        update = {
+            '$set': dict(
+                status='error',
+                compute_resource_status='error',
+                result=None,
+                runtime_info=runtime_info,
+                exception='{}'.format(exception),
+                last_modified_by_compute_resource=True
+            )
+        }
+        db.update_one(filter0, update=update)
+    
+    def _report_active(self):
+        db = self._get_db(collection='active_compute_resources')
+        filter = dict(
+            compute_resource_id=self._compute_resource_id
+        )
+        update = {
+            '$set': dict(
+                compute_resource_id=self._compute_resource_id,
+                utctime=_utctime()
+            )
+        }
+        db.update_one(filter, update=update, upsert=True)
+
+    def _get_db(self, collection='hither2_jobs'):
         import pymongo
         url = self._mongo_url
         if url != self._client_db_url:
@@ -73,6 +194,9 @@ class ComputeResource:
                 self._client.close()
             self._client = pymongo.MongoClient(url, retryWrites=False)
             self._url = url
-        return self._client[self._database]['hither2_jobs']
+        return self._client[self._database][collection]
 
     
+def _utctime():
+    from datetime import datetime, timezone
+    return datetime.utcnow().replace(tzinfo=timezone.utc).timestamp()
--- hither2/remotejobhandler.py
+++ hither2/remotejobhandler.py
@@ -7,6 +7,8 @@ from ._util import _random_string
 
 class RemoteJobHandler:
     def __init__(self, *, mongo_url, database, compute_resource_id):
+        self.is_remote = True
+        
         self._mongo_url = mongo_url
         self._database = database
         self._compute_resource_id = compute_resource_id
@@ -17,6 +19,8 @@ class RemoteJobHandler:
         self._iterate_timer = time.time()
 
     def handle_job(self, job):
+        self._report_active()
+
         job_serialized = job._serialize(generate_code=True)
         job_serialized['code'] = ka.store_object(job_serialized['code'])
         db = self._get_db()
@@ -26,6 +30,7 @@ class RemoteJobHandler:
             job_id=job._job_id,
             job_serialized=job_serialized,
             status='queued',
+            compute_resource_status='pending',
             runtime_info=None,
             result=None,
             last_modified_by_compute_resource=False,
@@ -38,6 +43,9 @@ class RemoteJobHandler:
         elapsed = time.time() - self._iterate_timer
         if elapsed < 3:
             return
+
+        self._report_active()
+
         self._iterate_timer = time.time()
         db = self._get_db()
         client_code = _random_string(15)
@@ -57,19 +65,43 @@ class RemoteJobHandler:
             job_id = doc['job_id']
             if job_id in self._jobs:
                 j = self._jobs[job_id]
-                j._runtime_info = doc['runtime_info']
-                j._status = doc['status']
-                if doc['status'] == 'finished':
+                compute_resource_status = doc['compute_resource_status']
+                if compute_resource_status == 'queued':
+                    print(f'Job queued: {job_id}')
+                elif compute_resource_status == 'running':
+                    print(f'Job queued: {job_id}')
+                elif compute_resource_status == 'finished':
+                    print(f'Job finished: {job_id}')
+                    j._runtime_info = doc['runtime_info']
+                    j._status = 'finished'
                     j._result = _deserialize_item(doc['result'])
                     del self._jobs[job_id]
-                elif doc['status'] == 'error':
-                    j._exception = _deserialize_item(doc['exception'])
+                elif compute_resource_status == 'error':
+                    print(f'Job error: {job_id}')
+                    j._runtime_info = doc['runtime_info']
+                    j._status = 'error'
+                    j._exception = Exception(doc['exception'])
                     del self._jobs[job_id]
+                else:
+                    raise Exception(f'Unexpected compute resource status: {compute_resource_status}')
+    
+    def _report_active(self):
+        db = self._get_db(collection='active_job_handlers')
+        filter = dict(
+            handler_id=self._handler_id
+        )
+        update = {
+            '$set': dict(
+                handler_id=self._handler_id,
+                utctime=_utctime()
+            )
+        }
+        db.update_one(filter, update=update, upsert=True)
 
     def cleanup(self):
         pass
 
-    def _get_db(self):
+    def _get_db(self, collection='hither2_jobs'):
         import pymongo
         url = self._mongo_url
         if url != self._client_db_url:
@@ -77,4 +109,8 @@ class RemoteJobHandler:
                 self._client.close()
             self._client = pymongo.MongoClient(url, retryWrites=False)
             self._url = url
-        return self._client[self._database]['hither2_jobs']
+        return self._client[self._database][collection]
+
+def _utctime():
+    from datetime import datetime, timezone
+    return datetime.utcnow().replace(tzinfo=timezone.utc).timestamp()
