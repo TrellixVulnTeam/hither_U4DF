2284f7bb91f5b2a8a26f36e963148d81ebd7b60b
Jeremy Magland <jmagland@flatironinstitute.org>
2020-02-27 15:52:12 -0500

    travis deploy to pypi

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ pkg_name = "hither2"
 
 setuptools.setup(
     name=pkg_name,
-    version="0.1.0",
+    version="0.1.1",
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="Run batches of Python functions in containers and on remote servers",
     packages=setuptools.find_packages(),
     scripts=[],
     install_requires=[
+        "pymongo"
+        # non-explicit dependencies: kachery, numpy
     ],
     classifiers=(
         "Programming Language :: Python :: 3",
